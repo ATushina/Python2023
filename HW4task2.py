47ffb69be2495845086a47a44be1a29b81b605b3
ATushina <115179514+ATushina@users.noreply.github.com>
2024-01-09 10:29:54 +0300

    Add files via upload
    HW4

---
+++ HW4task2.py
@@ -0,0 +1,15 @@
+#Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, 
+# где ключ — значение переданного аргумента, а значение — имя аргумента. 
+#Если ключ не хешируем, используйте его строковое представление.
+
+def dict_key(data):
+     dict_new = {}
+     if type(data) == list or type(data) == bytearray or type(data) == dict or type(data) == frozenset:
+         dict_new[str(data)] = type(data)
+         return dict_new
+     else:
+         dict_new[data] = type(data)
+         return dict_new
+
+
+print(dict_key({1: 2}))
